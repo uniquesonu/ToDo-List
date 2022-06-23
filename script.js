fca0b90bbdf4fdb625fa7124452419c7d98d2231
uniquesonu <its.sonu832@gmail.com>
2022-06-23 20:47:01 +0530

    ToDo List

---
+++ script.js
@@ -0,0 +1,20 @@
+// let input = document.querySelector('input');
+// let button = document.querySelector('button');
+// let ul = document.querySelector('ul');
+
+// let li = document.querySelector('li');
+
+// let handleClick = (e) => {
+//     let CurrTask = input.value;
+//     if(CurrTask.length > 0 && CurrTask.trim()!==''){
+//         ul.innerHTML += '<li>' + CurrTask + '</li>';
+//     }
+//     input.value = '';
+// };
+
+// button.addEventListener('click',handleClick); 
+
+let newTask = document.querySelector('.container input');
+let addButton = document.querySelector('.container button');
+
+let taskList = document.querySelector('')
